52ac7665f09a3182abf5da4843604784d7451e5c
NohaAdel32 <love.queen1921989@gmail.com>
2023-10-04 16:00:18 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for x in range(97, 123):
-    if (x==101 or x==113):
-        continue
-    print("{:c}".format(x), end="")
+    if (x!=101 and x!=113):
+        print("{:c}".format(x), end="")
