55bda5291deb240f1c1693ad14b19eaf90a9aba7
NohaAdel32 <love.queen1921989@gmail.com>
2023-10-04 16:24:12 -0700

    7-islower.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for x in range(97, 123):
-    if (x!=101 and x!=113):
-        print("{:c}".format(x), end="")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
