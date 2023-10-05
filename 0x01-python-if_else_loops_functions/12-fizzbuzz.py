898a2e1adc5210cb9cc13300556d28cfa77e10e8
NohaAdel32 <love.queen1921989@gmail.com>
2023-10-04 17:51:43 -0700

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 def fizzbuzz():
-    for num in range(1, 101):
-        if num % 15 == 0:
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz ", end="")
-        elif num % 3 == 0:
+        elif number % 3 == 0:
             print("Fizz ", end="")
-        elif num % 5 == 0:
+        elif number % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{:d} ".format(num), end="")
-
+            print("{} ".format(number), end="")
